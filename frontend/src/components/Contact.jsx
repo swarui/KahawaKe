781abf28c238f22c1090b11137fd1def84f012d1
swarui <stevewarui88@gmail.com>
2025-04-03 23:38:18 +0300

    Kahawa ke

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -30,7 +30,7 @@ const ContactSection = () => {
     setIsSending(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/contact", {
+      const response = await fetch("https://kahawakedualboot1.onrender.com/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
